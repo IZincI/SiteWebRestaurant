0793dce669365a0f1dc56184f0291ec565fcc9c0
Zinc <geoffrey_02@outlook.fr>
2022-07-01 10:34:09 +0200

    seconde update

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-    let SalleInterieure=document.getElementById("SalleInterieure");
-
-    SalleInterieure.addEventListener("mouseover",function(event)){
-        event.target.style.color = "orange";
-    }
