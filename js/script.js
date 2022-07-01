92585806d88011b5dc99d2a96cafaba79f5faf30
Zinc <geoffrey_02@outlook.fr>
2022-07-01 09:13:51 +0200

    finalisation html css

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+    let SalleInterieure=document.getElementById("SalleInterieure");
+
+    SalleInterieure.addEventListener("mouseover",function(event)){
+        event.target.style.color = "orange";
+    }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+le projet consiste a utiliser GIT
+il y aura du html css javascript
